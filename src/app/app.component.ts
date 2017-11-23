4394d91b44be59d64fb6ffaef669d2ce1d5f478b
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-11-23 03:06:14 +0100

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular app';
+
+  post = {
+    title: "Title",
+    isFavorite: true
+  }
+
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs){
+    console.log("favorite changed: ", eventArgs);
+  }
+  tweet = {
+    body: "",
+    likesCount: 10,
+    isLiked: true
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { PanelComponent } from './panel/panel.component';
 import { CoursesComponent } from './courses.component';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { LikeComponent } from './like/like.component';
+
 
 
 
@@ -18,7 +22,10 @@ import { AuthorsComponent } from './authors/authors.component';
     AppComponent,
     CourseComponent,
     CoursesComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    FavoriteComponent,
+    PanelComponent,
+    LikeComponent
     
   ],
   imports: [
